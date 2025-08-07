702746ce5e03efd4643f9ce2e37e11ffc6928acf
daebeom.cho <daebeom.cho@t-ime.com>
2025-08-07 11:45:31 +0900

    feat: layout.tsx 문구 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "소확행 - 소개팅 서비스",
-  description: "2025년 8월 한정 초특가 이벤트 진행 중!",
+  title: "소확행💘 - 소소하지만 확실한, 행복한 만남 💕",
+  description: "2025년 8월 초특가 이벤트 진행 중!",
 };
 
 export default function RootLayout({
